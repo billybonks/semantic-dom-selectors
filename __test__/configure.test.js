11f9a1af99ccf0a62b0338a2f376c55eb336632c
billybonks <sebastienstettler@gmail.com>
2018-08-05 02:36:44 +0800

    [TEST] Add configure function test

--- __test__/configure.test.js
+++ __test__/configure.test.js
@@ -1,5 +1,5 @@
 import config from '../src/config';
-import { _merge } from '../src/configure';
+import configure from '../src/configure';
 import presetDefault from '../src/presets/default';
 
 describe('Configuration Module', () => {
@@ -35,7 +35,7 @@ describe('Configuration Module', () => {
       });
       describe('#_merge', () => {
         beforeEach(() => {
-          result = _merge(cusatomConfig);
+          result = configure._merge(cusatomConfig);
         });
 
         test('merges actors appropriately', () => {
@@ -74,9 +74,53 @@ describe('Configuration Module', () => {
         ],
       };
     });
+
+    describe('#configure', () => {
+      const originalMerge = configure._merge;
+      const orignalRegisterActor = config.registerActor;
+      const orignalRegisterFinder = config.registerFinder;
+      const orignalSetErrorLevels = config.setErrorLevels;
+
+      beforeEach(() => {
+        configure._merge = jest.fn().mockReturnValue(cusatomConfig);
+        config.registerActor = jest.fn();
+        config.registerFinder = jest.fn();
+        config.setErrorLevels = jest.fn();
+        configure.configure(cusatomConfig);
+      });
+
+      afterEach(() => {
+        configure._merge = originalMerge;
+        config.registerActor = orignalRegisterActor;
+        config.registerFinder = orignalRegisterFinder;
+        config.setErrorLevels = orignalSetErrorLevels;
+      });
+
+      it('merges the config', () => {
+        expect(configure._merge).toHaveBeenCalledTimes(1);
+      });
+
+      it('Calls registerActor with all actors', () => {
+        expect(config.registerActor).toHaveBeenNthCalledWith(1, cusatomConfig.actors[0]);
+        expect(config.registerActor).toHaveBeenNthCalledWith(2, cusatomConfig.actors[1]);
+        expect(config.registerActor).toHaveBeenCalledTimes(2);
+      });
+
+      it('Calls registerFinder with all finders', () => {
+        expect(config.registerFinder).toHaveBeenNthCalledWith(1, cusatomConfig.finders[0]);
+        expect(config.registerFinder).toHaveBeenNthCalledWith(2, cusatomConfig.finders[1]);
+        expect(config.registerFinder).toHaveBeenCalledTimes(2);
+      });
+
+      it('sets ErrorLevels', () => {
+        expect(config.setErrorLevels).toHaveBeenCalledWith(cusatomConfig.rules);
+        expect(config.setErrorLevels).toHaveBeenCalledTimes(1);
+      });
+    });
+
     describe('#_merge', () => {
       beforeEach(() => {
-        result = _merge(cusatomConfig);
+        result = configure._merge(cusatomConfig);
       });
       test('merges actors as is', () => {
         expect(result.actors).toEqual([...cusatomConfig.actors]);
