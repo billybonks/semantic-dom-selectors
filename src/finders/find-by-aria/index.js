ebc94da472a15aba71922a2cbf2fd1ad79239016
billybonks <sebastienstettler@gmail.com>
2020-01-19 18:53:31 +0800

    upgrade: babel/rollup/eslint/jest

--- src/finders/find-by-aria/index.js
+++ src/finders/find-by-aria/index.js
@@ -3,5 +3,5 @@ import findAll from '../helpers/findAll';
 import matchLabel from '../helpers/matchLabel';
 
 export default function findByAria(selector, labelText) {
-  return findAll(selector).filter(element => matchLabel(labelText, computeAria(element)));
+  return findAll(selector).filter((element) => matchLabel(labelText, computeAria(element)));
 }
