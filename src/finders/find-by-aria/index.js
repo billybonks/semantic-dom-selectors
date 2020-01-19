aaa37348a1443e093b98e3f9e2ae4bc1eb94ccae
Ryan Chang <ryancyq@gmail.com>
2020-01-19 17:14:24 +0800

    feat(finders): findByAria to support regex
    Feature enabled by using the matchLabel helper

--- src/finders/find-by-aria/index.js
+++ src/finders/find-by-aria/index.js
@@ -1,6 +1,7 @@
 import computeAria from './compute-aria';
 import findAll from '../helpers/findAll';
+import matchLabel from '../helpers/matchLabel';
 
 export default function findByAria(selector, labelText) {
-  return findAll(selector).filter(element => computeAria(element) === labelText.toLowerCase());
+  return findAll(selector).filter(element => matchLabel(labelText, computeAria(element)));
 }
