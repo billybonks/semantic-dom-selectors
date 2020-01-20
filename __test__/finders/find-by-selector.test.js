45449931136be259fcf912b9311f4e417a6c4cf1
billybonks <sebastienstettler@gmail.com>
2020-01-20 16:48:43 +0800

    test(finder): Add a test for findByQuery that asserts role
    since you are using a query selector we esnure that you get the element
    but we want to ensure that the role of the element is atleast correct.
    
    in the future we might improve this by adding hints
    "you could find it by x"

--- __test__/finders/find-by-selector.test.js
+++ __test__/finders/find-by-selector.test.js
@@ -1,30 +1,55 @@
 import findBySelector from '../../src/finders/find-by-selector';
 
 describe('Find by selector', () => {
-  beforeEach(() => {
-    const markup = `
-      <div class="parent">
-        <button id="ni" class="child" />
-      </div>
-    `;
-    document.body.innerHTML = markup;
+  describe('when dom is semantic', () => {
+    beforeEach(() => {
+      const markup = `
+        <div class="parent">
+          <button id="ni" class="child" />
+        </div>
+      `;
+      document.body.innerHTML = markup;
+    });
+
+    afterEach(() => {
+      document.body.innerHTML = '';
+    });
+
+    it('finds the child button', () => {
+      const expectedButton = document.querySelector('#ni');
+      const foundButton = findBySelector.run('button', '.child');
+      expect(foundButton.length).toEqual(1);
+      expect(foundButton[0]).toEqual(expectedButton);
+    });
+
+    it('finds the child button via parent', () => {
+      const expectedButton = document.querySelector('#ni');
+      const foundButton = findBySelector.run('.parent', '> .child');
+      expect(foundButton.length).toEqual(1);
+      expect(foundButton[0]).toEqual(expectedButton);
+    });
   });
 
-  afterEach(() => {
-    document.body.innerHTML = '';
-  });
+  describe('when dom is not semantic', () => {
+    beforeEach(() => {
+      const markup = `
+        <div class="parent">
+          <span id="ni" class="child" />
+        </div>
+      `;
+      document.body.innerHTML = markup;
+    });
 
-  it('finds the child button', () => {
-    const expectedButton = document.querySelector('#ni');
-    const foundButton = findBySelector.run('button', '.child');
-    expect(foundButton.length).toEqual(1);
-    expect(foundButton[0]).toEqual(expectedButton);
-  });
+    afterEach(() => {
+      document.body.innerHTML = '';
+    });
 
-  it('finds the child button via parent', () => {
-    const expectedButton = document.querySelector('#ni');
-    const foundButton = findBySelector.run('.parent', '> .child');
-    expect(foundButton.length).toEqual(1);
-    expect(foundButton[0]).toEqual(expectedButton);
+    it('finds the child button', () => {
+      try {
+        findBySelector.run('button', '.child');
+      } catch (e) {
+        expect(e.message).toEqual('Element found with invalid role');
+      }
+    });
   });
 });
