3a122fd0baebe39bc478a8b8836a0919e9dec372
billybonks <sebastienstettler@gmail.com>
2020-03-29 19:46:41 +0800

    docs: document configure function

--- src/configure.js
+++ src/configure.js
@@ -1,6 +1,31 @@
 import config from './config';
 import mergePresets from './config/merge-presets';
 
+/**
+ * Configures the library to behave in a certain way
+ *
+ *
+ * @param {object} configuration
+ *
+ * @example
+ * let config = {
+ *  preset: 'aria-compliant',
+ *  rules: {
+ *    'something-else': 1,
+ *    'invalid-label-for': 2,
+ *  },
+ *  finders: [
+ *    { key: 'asd', run() {} },
+ *    { key: 'qqq-www', run() {} },
+ *  ],
+ *  actors: [
+ *    { key: 'some-act', type: 'select', run() {} },
+ *    { key: 'some-input-act', type: 'input', run() {} },
+ *  ],
+ * };
+ *
+ * configure(config)
+ */
 export default function (configuration) {
   const normalizedConfig = mergePresets(configuration);
   config.reset();
