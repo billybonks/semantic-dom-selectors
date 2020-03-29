e7230f9935a01acfcc255764f34188ff47133e0c
billybonks <sebastienstettler@gmail.com>
2020-03-29 13:53:47 +0800

    style: auto lint files

--- __test__/notify.test.js
+++ __test__/notify.test.js
@@ -1,22 +1,21 @@
-import notify  from '../src/notify';
-import config  from '../src/config';
+import notify from '../src/notify';
+import config from '../src/config';
 
 function setALevel(rule, level) {
   config.registerFinder({
-    key: rule
-  })
+    key: rule,
+  });
   config.errorLevelOptions = {
     [rule]: level,
-  }
+  };
 }
 
-function resetConfig(){
+function resetConfig() {
   config.errorLevelOptions = {};
   config.registeredFinders = [];
 }
 
 describe('Notification module', () => {
-
   beforeEach(() => {
     jest.spyOn(console, 'warn');
   });
@@ -33,60 +32,58 @@ describe('Notification module', () => {
 
     test('Uses term element by default', () => {
       setALevel('test-rule', 1);
-      notify('test-rule')
+      notify('test-rule');
       expect(console.warn).toHaveBeenCalledWith("Custom rule test-rule found Element labelled 'undefined'");
-
     });
   });
 
   describe('Customisng the message', () => {
-
-    beforeEach( () => {
+    beforeEach(() => {
       setALevel('test-rule', 1);
-    })
+    });
 
     describe('Specifining type', () => {
       test('Type is uppercased and control is appended', () => {
-        notify('test-rule', 'button')
+        notify('test-rule', 'button');
         expect(console.warn).toHaveBeenCalledWith("Custom rule test-rule found Button Control labelled 'undefined'");
-      })
-    })
+      });
+    });
 
     describe('generateMessage', () => {
       test('it uses return of function instead', () => {
-        notify('test-rule', 'button', 'blank', function(){return 'troll'})
-        expect(console.warn).toHaveBeenCalledWith("troll");
-      })
-    })
+        notify('test-rule', 'button', 'blank', () => 'troll');
+        expect(console.warn).toHaveBeenCalledWith('troll');
+      });
+    });
 
     describe('labelText', () => {
-      test('it includes labelText in message as is',  () => {
-        notify('test-rule', 'button', 'save')
+      test('it includes labelText in message as is', () => {
+        notify('test-rule', 'button', 'save');
         expect(console.warn).toHaveBeenCalledWith("Custom rule test-rule found Button Control labelled 'save'");
-      })
-    })
+      });
+    });
   });
 
   describe('Notification levels', () => {
     describe('level is 0', () => {
-      it('throws an error', function(){
+      it('throws an error', () => {
         setALevel('test-rule', 0);
         expect(() => {
-          notify('test-rule')
+          notify('test-rule');
         }).toThrow();
       });
     });
 
     describe('level is 1', () => {
-      it('logs to console.warn', function(){
+      it('logs to console.warn', () => {
         setALevel('test-rule', 1);
-        notify('test-rule')
+        notify('test-rule');
         expect(console.warn).toHaveBeenCalled();
       });
     });
 
     describe('level is 2', () => {
-      it('does nothing', function(){
+      it('does nothing', () => {
         setALevel('test-rule', 2);
         expect(console.warn).not.toHaveBeenCalled();
       });
